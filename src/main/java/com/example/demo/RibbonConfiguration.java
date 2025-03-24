ad2d74ca48d2efa1aeda87aae618a1f10e72eb90
Alex Batac <alexbatac.ca@gmail.com>
2025-03-23 23:36:00 -0400

    Ribbon Configuration

--- src/main/java/com/example/demo/RibbonConfiguration.java
+++ src/main/java/com/example/demo/RibbonConfiguration.java
@@ -1,6 +1,13 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AvailabilityFilteringRule;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 public class RibbonConfiguration {
 		
